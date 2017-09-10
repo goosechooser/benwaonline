a1f53b7642060a1e17b10dd12dd072613b0a78e4
Marshall <xtremebass2k@gmail.com>
2017-09-10 02:53:58 -0700

    Added random names just like the old guestbook

--- benwaonline/gallery/gallery.py
+++ benwaonline/gallery/gallery.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 from flask import Blueprint, request, session, g, redirect, url_for, \
      render_template
@@ -7,6 +8,16 @@ from benwaonline.database import db
 from benwaonline.models import BenwaPicture, Benwa, GuestbookEntry
 
 gallery = Blueprint('gallery', __name__, template_folder='templates', static_folder='static', static_url_path='/static/gallery')
+
+NUM = ['420', '69']
+CONNECT = ['xXx', '_', '']
+ADJ = ['lover', 'liker', 'hater']
+
+@gallery.context_processor
+def inject_guestbook_info():
+    username = random.choice(CONNECT).join(['benwa', random.choice(ADJ), random.choice(NUM)])
+
+    return {'name' : username}
 
 @gallery.route('/gallery/')
 def display_benwas():
