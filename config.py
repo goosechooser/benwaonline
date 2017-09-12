0192864ecb8c2364a4064da3667e59e81c248dcf
Marshall <xtremebass2k@gmail.com>
2017-09-12 05:48:47 -0700

    Added STATIC_ROOT to config

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'benwaonline.db')
-DATABASE_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = 'dev key'
