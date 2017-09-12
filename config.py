5d81e08ecc36dcc9de0d3883f0ad2949fdbcd815
goosechooser <xtremebass2k@gmail.com>
2017-09-12 06:48:24 -0700

    Gallery + Rotating + Guestbook Merge (#2) (#3)
    Models/Database Related:
    * Added the Benwa model that encapsulates BenwaPicture and GuestBookEntry models
    * Added thumbnail column to BenwaPicture table
    * added 'addbenwas' to the flask cmdline
    ** lets you add benwas to the benwa database
    
    Gallery:
    * Main view now shows a thumbnail of every benwa
    * From there you have a fullsize benwa with ability to travel to the next/previous benwa and you can even leave a comment on the benwa
    * Added .css so gallery scales with device screen (need to test further)
    
    Misc:
    * Updated to match where static content is placed
    * Start of Alembic/Flask-Migrate
    * Oops should have opened a new branch before doing this.

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'benwaonline.db')
