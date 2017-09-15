4e8b2dcaae0f7af9b4f8f6218824f13ade34327a
Marshall <xtremebass2k@gmail.com>
2017-09-15 03:37:54 -0700

    Massive overhaul of database schema

--- benwaonline/gallery/gallery.py
+++ benwaonline/gallery/gallery.py
@@ -19,31 +19,48 @@ def inject_guestbook_info():
 
     return {'name' : username}
 
+#e621 does /post/
+# post/index/<int:page>/
+# post/index/<int:page>/<tags>
+# def display_posts():
+#     posts = Post.query.all()
+
+#     return render_template('gallery.html', posts=posts, tag=None)
+
+def process_post_query(model, tag):
+    posts = Post.query.all()
+    if tag:
+        posts = posts.filter(Post.tags.any(name=tag))
+        
+    return tag
 @gallery.route('/gallery/')
-@gallery.route('/gallery/<string:pool>/')
-def display_benwas(pool='benwas'):
-    pool = Pool.query.filter_by(name=pool).first()
-    benwas = pool.members()
+@gallery.route('/gallery/<string:tag>/')
+def display_posts(tag=None):
+    posts = Post.query.all()
+    if tag:
+        posts = posts.filter(Post.tags.any(name=tag))
+    print(posts)
 
-    return render_template('gallery.html', benwas=benwas, pool=pool)
+    return render_template('gallery.html', posts=posts, tag=tag)
 
-@gallery.route('/gallery/<string:pool>/<int:page>/add?<int:benwa_id>', methods=['POST'])
-def add_comment(page=1, pool='benwas', benwa_id=0):
+@gallery.route('/gallery/<string:tag>/<int:page>/add?<int:post_id>', methods=['POST'])
+def add_comment(page=1, tag='benwas', post_id=1):
     form = forms.GuestbookEntry(request.form)
 
     if form.validate():
-        owner = Benwa.query.filter_by(id=benwa_id).first()
-        entry = GuestbookEntry(name=form.name.data, content=form.content.data,\
-                date_posted=datetime.utcnow(), owner=owner)
+        post = Post.query.get(post_id)
+        comment = Comment(poster_name=form.name.data, content=form.content.data,\
+                created=datetime.utcnow(), post=post)
 
-        db.session.add(entry)
+        db.session.add(comment)
         db.session.commit()
 
-    return redirect(url_for('gallery.rotating', page=page, pool=pool))
+    return redirect(url_for('gallery.rotating', page=page, tag=tag))
 
-@gallery.route('/gallery/<string:pool>/<int:page>', methods=['GET', 'POST'])
-def rotating(page=1, pool='benwas'):
-    pool = Pool.query.filter_by(name=pool).first()
-    benwas = pool.benwas.paginate(page, 1, False)
+#https://e621.net/post/show/1330331/
+@gallery.route('/gallery/<string:tag>/<int:page>', methods=['GET', 'POST'])
+def rotating(page=1, tag='benwas'):
+    posts_q = Post.query.filter(Post.tags.any(name=tag))
+    posts = posts_q.paginate(page, 1, False)
 
-    return render_template('rotating.html', benwas=benwas, pool=pool)
+    return render_template('rotating.html', posts=posts, tag=tag)
