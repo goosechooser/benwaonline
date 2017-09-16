f996ae2efedcc9f97ea170fa396389fd211c718b
goosechooser <xtremebass2k@gmail.com>
2017-09-16 05:54:27 -0700

    Development tags (#5)
    * Redid the database schema
      * No more Benwa/BenwaPicture etc 😭
    
    * Foundation of rudiementary tagging system.
     * Currently tags are really only added at the creation of post, but that will be changed in the future.
    
    * Updated layout of most pages involved with Posts
      * Tags in a column to the left
      * Gallery of posts / a single post to the right
    
    * Refactored most/all of the jinja templates to use macros instead of 'includes'

--- benwaonline/gallery/gallery.py
+++ benwaonline/gallery/gallery.py
@@ -5,7 +5,7 @@ from flask import Blueprint, request, session, g, redirect, url_for, \
 
 from benwaonline import forms
 from benwaonline.database import db
-from benwaonline.models import BenwaPicture, Benwa, GuestbookEntry
+from benwaonline.models import Post, Tag, Comment
 
 gallery = Blueprint('gallery', __name__, template_folder='templates', static_folder='static/', static_url_path='/static/')
 
@@ -20,26 +20,66 @@ def inject_guestbook_info():
     return {'name' : username}
 
 @gallery.route('/gallery/')
-def display_benwas():
-    benwas = Benwa.query.all()
-    return render_template('gallery.html', benwas=benwas)
+@gallery.route('/gallery/<string:tags>/')
+def display_posts(tags='all'):
+    if tags == 'all':
+        posts = Post.query.all()
+    else:
+        split = tags.split(' ')
+        posts = []
+        for s in split:
+            results = Post.query.filter(Post.tags.any(name=s))
+            posts.extend(results)
 
-@gallery.route('/gallery/<int:page>/add', methods=['POST'])
-def add_comment(page=1):
-    print(page)
-    form = forms.GuestbookEntry(request.form)
-    owner = Benwa.query.filter_by(id=page).first()
+    tags = Tag.query.all()
+
+    return render_template('gallery.html', posts=posts, tags=tags)
+
+@gallery.route('/gallery/show/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+
+    return render_template('show.html', post=post)
+
+@gallery.route('/gallery/show/<int:post_id>/previous')
+def show_previous_post(post_id):
+    post = Post.query.order_by(Post.id.desc()).filter(Post.id < post_id).first()
+    if post:
+        return redirect(url_for('gallery.show_post', post_id=post.id))
+
+    return redirect(request.referrer)
+
+@gallery.route('/gallery/show/<int:post_id>/next')
+def show_next_post(post_id):
+    post = Post.query.order_by(Post.id.asc()).filter(Post.id > post_id).first()
+    if post:
+        return redirect(url_for('gallery.show_post', post_id=post.id))
+
+    return redirect(request.referrer)
+
+# Need to make this more generic
+@gallery.route('/gallery/show/<int:post_id>/add', methods=['POST'])
+def add_comment(post_id):
+    form = forms.Comment(request.form)
 
     if form.validate():
-        entry = GuestbookEntry(name=form.name.data, content=form.content.data,\
-                date_posted=datetime.utcnow(), owner=owner)
+        post = Post.query.get(post_id)
+        comment = Comment(poster_name=form.name.data, content=form.content.data,\
+                created=datetime.utcnow(), post=post)
 
-        db.session.add(entry)
+        db.session.add(comment)
         db.session.commit()
 
-    return redirect(url_for('gallery.rotating', page=owner.id))
+    return redirect(url_for('gallery.show_post', post_id=post.id))
 
-@gallery.route('/gallery/<int:page>', methods=['GET', 'POST'])
-def rotating(page=1):
-    benwas = Benwa.query.paginate(page, 1, False)
-    return render_template('rotating.html', benwas=benwas)
+# #https://e621.net/post/show/1330331/
+# @gallery.route('/gallery/<string:tags>/<int:page>/', methods=['GET', 'POST'])
+# def rotating(page=1, tags='all'):
+#     if tags == 'all':
+#         posts_q = Post.query
+#     else:
+#         posts_q = Post.query.filter(Post.tags.any(name=tags))
+
+#     posts = posts_q.paginate(page, 1, False)
+
+#     return render_template('rotating.html', posts=posts, tags=tags)
