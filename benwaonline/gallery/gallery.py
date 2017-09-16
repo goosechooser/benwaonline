b2edd273858b8a018645ba1c70bee5e6bffd43fa
Marshall <xtremebass2k@gmail.com>
2017-09-15 23:18:06 -0700

    Implemented a 'show' page to view a single benwa, will eventually replace 'rotating'

--- benwaonline/gallery/gallery.py
+++ benwaonline/gallery/gallery.py
@@ -22,29 +22,42 @@ def inject_guestbook_info():
 #e621 does /post/
 # post/index/<int:page>/
 # post/index/<int:page>/<tags>
-# def display_posts():
-#     posts = Post.query.all()
-
-#     return render_template('gallery.html', posts=posts, tag=None)
-
-def process_post_query(model, tag):
-    posts = Post.query.all()
-    if tag:
-        posts = posts.filter(Post.tags.any(name=tag))
-        
-    return tag
 @gallery.route('/gallery/')
-@gallery.route('/gallery/<string:tag>/')
-def display_posts(tag=None):
-    posts = Post.query.all()
-    if tag:
-        posts = posts.filter(Post.tags.any(name=tag))
-    print(posts)
+@gallery.route('/gallery/<string:tags>/')
+def display_posts(tags='all'):
+    print(tags)
+    if tags == 'all':
+        posts = Post.query.all()
+    else:
+        split = tags.split(' ')
+        print(split)
+        posts = []
+        for s in split:
+            posts.extend(Post.query.filter(Post.tags.any(name=s)))
 
-    return render_template('gallery.html', posts=posts, tag=tag)
+    return render_template('gallery.html', posts=posts, tags=tags)
 
-@gallery.route('/gallery/<string:tag>/<int:page>/add?<int:post_id>', methods=['POST'])
-def add_comment(page=1, tag='benwas', post_id=1):
+@gallery.route('/gallery/show/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get(post_id)
+
+    return render_template('show.html', post=post)
+
+@gallery.route('/gallery/show/<int:post_id>/previous')
+def show_previous_post(post_id):
+    post = Post.query.order_by(Post.id.desc()).filter(Post.id < post_id).first()
+
+    return  redirect(url_for('gallery.show_post', post_id=post.id))
+
+@gallery.route('/gallery/show/<int:post_id>/next')
+def show_next_post(post_id):
+    post = Post.query.order_by(Post.id.asc()).filter(Post.id > post_id).first()
+
+    return  redirect(url_for('gallery.show_post', post_id=post.id))
+
+# Need to make this more generic
+@gallery.route('/gallery/show/<int:post_id>/add', methods=['POST'])
+def add_comment(post_id):
     form = forms.GuestbookEntry(request.form)
 
     if form.validate():
@@ -55,12 +68,16 @@ def add_comment(page=1, tag='benwas', post_id=1):
         db.session.add(comment)
         db.session.commit()
 
-    return redirect(url_for('gallery.rotating', page=page, tag=tag))
+    return redirect(url_for('gallery.show_post', post_id=post.id))
 
-#https://e621.net/post/show/1330331/
-@gallery.route('/gallery/<string:tag>/<int:page>', methods=['GET', 'POST'])
-def rotating(page=1, tag='benwas'):
-    posts_q = Post.query.filter(Post.tags.any(name=tag))
-    posts = posts_q.paginate(page, 1, False)
+# #https://e621.net/post/show/1330331/
+# @gallery.route('/gallery/<string:tags>/<int:page>/', methods=['GET', 'POST'])
+# def rotating(page=1, tags='all'):
+#     if tags == 'all':
+#         posts_q = Post.query
+#     else:
+#         posts_q = Post.query.filter(Post.tags.any(name=tags))
 
-    return render_template('rotating.html', posts=posts, tag=tag)
+#     posts = posts_q.paginate(page, 1, False)
+
+#     return render_template('rotating.html', posts=posts, tags=tags)
