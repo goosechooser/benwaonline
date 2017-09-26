d080941d93f1841e3b2e7fefe4692a59781a5d98
Marshall <xtremebass2k@gmail.com>
2017-09-26 06:03:16 -0700

    Got OAuth working

--- benwaonline/blueprints/benwaonline.py
+++ benwaonline/blueprints/benwaonline.py
@@ -15,14 +15,18 @@ bp = Blueprint('benwaonline', __name__)
 
 @bp.route('/')
 def under_construction():
-    # return redirect(url_for('gallery.display_posts'))
+    # return redirect(url_for('gallery.show_posts'))
     return redirect(url_for('benwaonline.test'))
 
 @twitter.tokengetter
 def get_twitter_token():
-    if 'user' in session:
-        user = session['twitter_oauth']
-        return user.oauth_token_hash, user.oauth_secret_hash
+     if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
+
+@bp.before_request
+def before_request():
+    g.user = current_user
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -35,7 +39,9 @@ def load_user(user_id):
 # @bp.route('/login/auth', methods=['GET', 'POST'])
 @bp.route('/login/auth')
 def auth():
-    callback_url = url_for('benwaonline.oauthorized', next=request.args.get('next'))
+    if g.user.is_authenticated:
+        return redirect('gallery.show_posts')
+    callback_url = url_for('benwaonline.oauthorize', next=request.args.get('next'))
     return twitter.authorize(callback=callback_url or request.referrer or None)
 
 @bp.route('/logout')
@@ -52,50 +58,47 @@ def test():
     else:
         return "not logged in " + str(current_user.is_authenticated)
 
-@bp.route('/oauthorized')
-def oauthorized():
+@bp.route('/oauthorize')
+def oauthorize():
     resp = twitter.authorized_response()
     if not resp:
         flash(u'You denied the request to sign in.')
-        return redirect(url_for('gallery.display_posts'))
+        return redirect(url_for('gallery.show_posts'))
 
-    user, new = get_or_create_user(resp)
-    if new:
-        return redirect(url_for('benwaonline.signup'))
-    else:
+    session['twitter_oauth'] = resp
+
+    user_id = resp['user_id']
+    user = User.query.filter_by(user_id=user_id).first()
+
+    if user:
         login_user(user)
         next = request.args.get('next')
-
         flash('You were signed in as %s' % user.username)
         return redirect(url_for('benwaonline.test'))
+    else:
+        session['user_id'] = user_id
+        session['token'] = bcrypt.hash(resp['oauth_token'])
+        session['secret'] = bcrypt.hash(resp['oauth_token_secret'])
+        return redirect(url_for('benwaonline.signup'))
 
 @bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         username = ''.join([form.adj.data, form.benwa.data, form.pos.data])
-        instance = user_datastore.create_user(user_id=g.session['user_id'], username=username,\
-            oauth_token_hash=g.session['token'], oauth_secret_hash=g.session['secret'])
+        name_exists = User.query.filter(User.username == username).all()
+        if name_exists:
+            flash('Username %s already in use' % username)
+            return redirect(url_for('benwaonline.signup'))
+
+        user = user_datastore.create_user(user_id=session['user_id'], username=username,\
+                oauth_token_hash=session['token'], oauth_secret_hash=session['secret'])
         db.session.commit()
 
-        login_user(instance)
+        login_user(user)
         next = request.args.get('next')
 
-        flash('You were signed in as %s' % username)
+        flash('You were signed in as %s' % user.username)
         return redirect(url_for('benwaonline.test'))
 
     return render_template('signup.html', form=form)
-
-def get_or_create_user(response):
-    uid = response['user_id']
-    instance = User.query.filter_by(user_id=uid).first()
-
-    if instance:
-        return instance, False
-    else:
-        g.session = {}
-        g.session['user_id'] = uid
-        g.session['token'] = bcrypt.hash(response['oauth_token'])
-        g.session['secret'] = bcrypt.hash(response['oauth_token_secret'])
-
-        return None, True
--- tests/test_benwaonline.py
+++ tests/test_benwaonline.py
@@ -3,7 +3,7 @@ from flask import g, url_for
 from passlib.hash import bcrypt
 from benwaonline.models import User, user_datastore
 from benwaonline.oauth import twitter
-from benwaonline.blueprints.benwaonline import get_or_create_user
+from benwaonline.blueprints.benwaonline import get_twitter_token
 
 # Create a user to test with
 def create_user(client, session):
@@ -23,25 +23,47 @@ def test_login_auth(client):
     assert response.status_code == 302
     assert twitter.authorize_url in response.headers['Location']
 
-def test_oauthorized(client, mocker, session):
+def test_oauthorize(client, mocker, session):
     from flask_login import current_user
     resp = {'x_auth_expires': '0', 'oauth_token_secret': 'secret',
             'user_id': '420', 'oauth_token': '59866969-token', 'screen_name': 'tester'}
 
-    # Test where we received a response
+    # Test where we received a response and user doesn't exist
     mocker.patch('benwaonline.oauth.twitter.authorized_response', return_value=resp)
-    response = client.get(url_for('benwaonline.oauthorized'), follow_redirects=False)
+    response = client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
     assert response.status_code == 302
     assert 'signup' in response.headers['Location']
 
+    # Test get twitter token
+    token, secret = get_twitter_token()
+    assert token == resp['oauth_token']
+    assert secret == resp['oauth_token_secret']
+
+    # Test signup
+    form = {'adj': 'beautiful', 'benwa': 'benwa', 'pos': 'lover', 'submit': True}
+    response = client.post('/signup', data=form, follow_redirects=True)
+    assert response.status_code == 200
+
+    # Test to make sure the user made it to the database
+    query = User.query.first()
+    assert query.username == ''.join([form['adj'], form['benwa'], form['pos']])
+    assert query.user_id == '420'
+
     # Test where we receive a response and the user already exists
-    test_user = create_user(client, session)
-    response = client.get(url_for('benwaonline.oauthorized'), follow_redirects=False)
+    response = client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
     assert response.status_code == 302
     assert 'test' in response.headers['Location']
 
+    # Checking signup handles an existing user correctly
+    response = client.post('/signup', data=form, follow_redirects=False)
+    assert response.status_code == 302
+    assert 'signup' in response.headers['Location']
+
     # Test that we are logged in
     assert current_user.is_authenticated
+    response = client.get(url_for('benwaonline.auth', follow_redirects=False))
+    assert response.status_code == 302
+    assert 'gallery' in response.headers['Location']
 
     # Test log out
     response = client.get(url_for('benwaonline.logout'), follow_redirects=True)
@@ -50,49 +72,6 @@ def test_oauthorized(client, mocker, session):
 
     # Test where user denied / didnt receive a response
     mocker.patch('benwaonline.oauth.twitter.authorized_response', return_value=None)
-    response = client.get(url_for('benwaonline.oauthorized'), follow_redirects=False)
+    response = client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
     assert response.status_code == 302
     assert 'gallery' in response.headers['Location']
-
-def test_get_or_create_user(client, session):
-    resp = {'x_auth_expires': '0', 'oauth_token_secret': 'super',
-        'user_id': '420', 'oauth_token': '59866969-token',
-        'screen_name': 'tester'}
-
-    # Its a new user
-    user, new = get_or_create_user(resp)
-    assert not user
-    assert new
-    assert g.session['user_id'] == resp['user_id']
-    assert bcrypt.verify(resp['oauth_token'], g.session['token'])
-    assert bcrypt.verify(resp['oauth_token_secret'], g.session['secret'])
-
-    # Its not a new user
-    test_user = create_user(client, session)
-    user, new = get_or_create_user(resp)
-    assert not new
-    assert user == test_user
-
-def test_signup(client, app, session):
-    # Test POST - do we get redirected?
-    response = client.get(url_for('benwaonline.signup'), follow_redirects=False)
-    assert response.status_code == 200
-    assert b'<form action="" method="post" name="RegistrationForm"' in response.data
-
-    # Test POST with fields
-    form = {'adj': 'beautiful', 'benwa': 'benwa', 'pos': 'liker', 'submit': True}
-    mock_oauth_response = {'x_auth_expires': '0', 'oauth_token_secret': 'super',
-        'user_id': '420', 'oauth_token': '59866969-token',
-        'screen_name': 'tester'}
-
-    get_or_create_user(mock_oauth_response)
-    response = client.post('/signup', data=form, follow_redirects=False)
-
-    # Test that we were redirected to the right place
-    assert response.status_code == 302
-    assert 'test' in response.headers['Location']
-
-    # Test to make sure the user made it to the database
-    query = User.query.first()
-    assert query.username == ''.join([form['adj'], form['benwa'], form['pos']])
-    assert query.user_id == '420'
