ca836cd2417abf1931fe0b732bbea4babc47c54e
Marshall <xtremebass2k@gmail.com>
2017-09-26 06:41:52 -0700

    Refactored + added tests * renamed 'display_posts' to 'show_posts' * using pagination again * deleted 'rotating.html'

--- benwaonline/gallery/gallery.py
+++ benwaonline/gallery/gallery.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from flask import Blueprint, request, redirect, url_for, render_template
-from flask_security import login_required
+from flask import Blueprint, request, redirect, url_for, render_template, flash
+from flask_security import login_required, current_user
 
 from benwaonline import forms
 from benwaonline.database import db
@@ -10,7 +10,7 @@ gallery = Blueprint('gallery', __name__, template_folder='templates')
 
 @gallery.route('/gallery/')
 @gallery.route('/gallery/<string:tags>/')
-def display_posts(tags='all'):
+def show_posts(tags='all'):
     if tags == 'all':
         posts = Post.query.all()
     else:
@@ -24,40 +24,33 @@ def display_posts(tags='all'):
 
     return render_template('gallery.html', posts=posts, tags=tags)
 
+@gallery.route('/gallery/show/')
+def show_post_redirect():
+    return redirect(url_for('gallery.show_posts'))
+
 @gallery.route('/gallery/show/<int:post_id>')
 def show_post(post_id):
-    post = Post.query.get(post_id)
-
-    return render_template('show.html', post=post)
-
-@gallery.route('/gallery/show/<int:post_id>/previous')
-def show_previous_post(post_id):
-    post = Post.query.order_by(Post.id.desc()).filter(Post.id < post_id).first()
-    if post:
-        return redirect(url_for('gallery.show_post', post_id=post.id))
-
-    return redirect(request.referrer)
-
-@gallery.route('/gallery/show/<int:post_id>/next')
-def show_next_post(post_id):
-    post = Post.query.order_by(Post.id.asc()).filter(Post.id > post_id).first()
-    if post:
-        return redirect(url_for('gallery.show_post', post_id=post.id))
-
-    return redirect(request.referrer)
+    post = Post.query.paginate(post_id, 1, False)
+    if post.items:
+        return render_template('show.html', post=post)
+    else:
+        flash('That Benwa doesn\'t exist yet')
+        return redirect(url_for('gallery.show_posts'))
 
 # Need to make this more generic
 @gallery.route('/gallery/show/<int:post_id>/add', methods=['POST'])
 @login_required
 def add_comment(post_id):
     form = forms.Comment(request.form)
-
+    print(form.validate())
     if form.validate():
         post = Post.query.get(post_id)
-        comment = Comment(poster_name=form.name.data, content=form.content.data,\
-                created=datetime.utcnow(), post=post)
-
+        comment = Comment(content=form.content.data,\
+                created=datetime.utcnow())
         db.session.add(comment)
+
+        post.comments.append(comment)
+        current_user.comments.append(comment)
         db.session.commit()
 
     return redirect(url_for('gallery.show_post', post_id=post.id))
--- tests/test_gallery.py
+++ tests/test_gallery.py
@@ -1,3 +1,101 @@
+from datetime import datetime
+import pytest
+from flask import g, url_for
+from benwaonline.models import Post, Tag, User
+
+def test_show_posts(client, session):
+    # tag_benwa = Tag(name='benwa')
+    # tag_old_benwa = Tag(name='old_benwa')
+    # session.add(tag_benwa)
+    # session.add(tag_old_benwa)
+
+    # post = Post(title='test', created=datetime.utcnow(), tags=[tag_benwa])
+    # session.add(post)
+
+    # post2 = Post(title='test', created=datetime.utcnow(), tags=[tag_old_benwa])
+    # session.add(post2)
+
+    response = client.get('/gallery')
+    assert response.status_code == 301
+    assert 'gallery' in response.headers['Location']
+
+    response = client.get('/gallery/')
+    assert response.status_code == 200
+
+    response = client.get('/gallery/benwa')
+    assert response.status_code == 301
+    assert 'gallery/benwa' in response.headers['Location']
+
+    response = client.get('/gallery/benwa oldbenwa', follow_redirects=True)
+    assert response.status_code == 200
+
+    response = client.get('/gallery/benwa oldbenwa')
+    assert response.status_code == 301
+    assert 'gallery/benwa%20oldbenwa' in response.headers['Location']
+
+def test_show_post_redirect(client, session):
+    response = client.get('/gallery/show', follow_redirects=True)
+    assert response.status_code == 200
+
+    response = client.get('/gallery/show', follow_redirects=False)
+    assert response.status_code == 301
+    assert 'gallery/' in response.headers['Location']
+
+def test_show_post(client, session):
+    response = client.get('/gallery/show/1', follow_redirects=False)
+    assert response.status_code == 302
+    assert 'gallery/' in response.headers['Location']
+
+    post = Post(title='test', created=datetime.utcnow())
+    session.add(post)
+
+    response = client.get('/gallery/show/1')
+    assert response.status_code == 200
+
+# This may be too big for a unit test?
+def test_add_comment(client, session, mocker):
+    # Set up post
+    post = Post(title='test', created=datetime.utcnow())
+    session.add(post)
+
+    # Set up a user
+    resp = {'x_auth_expires': '0', 'oauth_token_secret': 'secret',
+            'user_id': '420', 'oauth_token': '59866969-token', 'screen_name': 'tester'}
+
+    mocker.patch('benwaonline.oauth.twitter.authorized_response', return_value=resp)
+    response = client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
+
+    form = {'adj': 'beautiful', 'benwa': 'benwa', 'pos': 'lover', 'submit': True}
+    response = client.post('/signup', data=form, follow_redirects=True)
+
+    comment = {'content': 'test comment'}
+    response = client.post('/gallery/show/1/add', data=comment)
+
+    user = User.query.first()
+    user_comment = user.comments.one()
+    assert user_comment.content == 'test comment'
+
+    post_comment = post.comments.one()
+    assert post_comment.content == 'test comment'
+
+    assert user_comment == post_comment
+
+# def test_show_post_previous_next(client, session):
+#     post = Post(title='test', created=datetime.utcnow())
+#     session.add(post)
+
+#     post = Post(title='test2', created=datetime.utcnow())
+#     session.add(post)
+
+#     response = client.get('/gallery/show/1/next')
+#     assert response.status_code == 302
+#     assert 'gallery/show/2' in response.headers['Location']
+
+#     response = client.get('/gallery/show/2/previous')
+#     assert response.status_code == 302
+#     assert 'gallery/show/1' in response.headers['Location']
+
+
 # def test_add_post(client, session):
 #     img_path = 'benwaonline/static/benwas/test.png'
 #     preview_path = 'benwaonline/static/benwas/preview.png'
