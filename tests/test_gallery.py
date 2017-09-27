5473920ab0031f64646d88e994b0576cde78e648
Marshall <xtremebass2k@gmail.com>
2017-09-27 01:01:30 -0700

    clean up

--- tests/test_gallery.py
+++ tests/test_gallery.py
@@ -1,20 +1,9 @@
 from datetime import datetime
 import pytest
-from flask import g, url_for
-from benwaonline.models import Post, Tag, User
+from flask import url_for
+from benwaonline.models import Post, User
 
 def test_show_posts(client, session):
-    # tag_benwa = Tag(name='benwa')
-    # tag_old_benwa = Tag(name='old_benwa')
-    # session.add(tag_benwa)
-    # session.add(tag_old_benwa)
-
-    # post = Post(title='test', created=datetime.utcnow(), tags=[tag_benwa])
-    # session.add(post)
-
-    # post2 = Post(title='test', created=datetime.utcnow(), tags=[tag_old_benwa])
-    # session.add(post2)
-
     response = client.get('/gallery')
     assert response.status_code == 301
     assert 'gallery' in response.headers['Location']
@@ -58,18 +47,20 @@ def test_add_comment(client, session, mocker):
     post = Post(title='test', created=datetime.utcnow())
     session.add(post)
 
-    # Set up a user
+    # Set up mock oauth
     resp = {'x_auth_expires': '0', 'oauth_token_secret': 'secret',
             'user_id': '420', 'oauth_token': '59866969-token', 'screen_name': 'tester'}
 
     mocker.patch('benwaonline.oauth.twitter.authorized_response', return_value=resp)
-    response = client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
+    client.get(url_for('benwaonline.oauthorize'), follow_redirects=False)
 
+    # Set up user
     form = {'adj': 'beautiful', 'benwa': 'benwa', 'pos': 'lover', 'submit': True}
-    response = client.post('/signup', data=form, follow_redirects=True)
+    client.post('/signup', data=form, follow_redirects=True)
 
+    # Set up comment
     comment = {'content': 'test comment'}
-    response = client.post('/gallery/show/1/add', data=comment)
+    client.post('/gallery/show/1/add', data=comment)
 
     user = User.query.first()
     user_comment = user.comments.one()
@@ -79,49 +70,3 @@ def test_add_comment(client, session, mocker):
     assert post_comment.content == 'test comment'
 
     assert user_comment == post_comment
-
-# def test_show_post_previous_next(client, session):
-#     post = Post(title='test', created=datetime.utcnow())
-#     session.add(post)
-
-#     post = Post(title='test2', created=datetime.utcnow())
-#     session.add(post)
-
-#     response = client.get('/gallery/show/1/next')
-#     assert response.status_code == 302
-#     assert 'gallery/show/2' in response.headers['Location']
-
-#     response = client.get('/gallery/show/2/previous')
-#     assert response.status_code == 302
-#     assert 'gallery/show/1' in response.headers['Location']
-
-
-# def test_add_post(client, session):
-#     img_path = 'benwaonline/static/benwas/test.png'
-#     preview_path = 'benwaonline/static/benwas/preview.png'
-#     add_post(img_path, preview_path)
-
-#     post = Post.query.first()
-
-#     # Dog I hate paths
-#     # assert post.image.filepath == os.path.relpath(img_path, 'benwaonline/static/')
-#     # assert post.preview.filepath == preview_path
-#     # Figure out better way to check if List of tags contains name of
-#     assert post.tags[0].name == 'benwa'
-
-# def test_add_posts(session):
-#     img_path = 'benwaonline/static/benwas/imgs'
-#     preview_path = 'benwaonline/static/benwas/thumbs'
-#     add_posts(img_path, preview_path)
-
-#     posts = Post.query.all()
-#     for post in posts:
-#         print(post.tags.count)
-#         _, img_tail = os.path.split(post.image.filepath)
-#         print(img_tail)
-#         _, preview_tail = os.path.split(post.preview.filepath)
-#         print(preview_tail)
-
-#         assert img_tail == preview_tail
-#         # assert post.preview.filepath == preview_path
-#         assert post.tags[0].name == 'benwa'
