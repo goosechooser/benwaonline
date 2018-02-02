984c2114049071a0b6b30312315f6c5e19afd87b
Marshall <xtremebass2k@gmail.com>
2018-02-02 03:55:28 -0800

    No longer need to minify css here since pagespeed does it for us

--- benwaonline/assets.py
+++ benwaonline/assets.py
@@ -1,5 +1,5 @@
 from flask_assets import Environment, Bundle
 assets = Environment()
 
-css = Bundle('style.css', filters='cssmin', output='min_style.%(version)s.css')
+css = Bundle('style.css', output='style_busted.%(version)s.css')
 assets.register('css_min', css)
