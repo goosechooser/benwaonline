c19aa8d603d2c9c1856834953ed3af4cc22de8f2
Marshall <xtremebass2k@gmail.com>
2018-02-20 01:18:09 -0800

    fixed the search bar and stylized the front page

--- benwaonline/front/views.py
+++ benwaonline/front/views.py
@@ -1,12 +1,16 @@
+from pathlib import Path
+from random import choice
 import json
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, current_app
 from benwaonline.back import back
 from benwaonline.front import front
 from benwaonline.front.forms import SearchForm
-# @bp.route('/')
-# @back.anchor
-# def under_construction():
-#     return render_template('index.html')
+
+def load_backgrounds(folder):
+    front = Path(current_app.static_folder)
+    front = front / folder
+    frontwas = [folder + '/' + f.name for f in front.iterdir()]
+    return frontwas
 
 @front.route('/faq')
 def faq():
@@ -16,12 +20,14 @@ def faq():
 
 @front.route('/', methods=['GET', 'POST'])
 def search():
+    bg_img = choice(load_backgrounds('imgs'))
+
     form = SearchForm()
     if form.validate_on_submit():
-        if any(form.tags.data):
+        if form.tags.data:
             joined = ' '.join(form.tags.data)
         else:
             joined = None
         return redirect(url_for('gallery.show_posts', tags=joined))
 
-    return render_template('search.html', form=form)
+    return render_template('front.html', form=form, bg_img=bg_img)
