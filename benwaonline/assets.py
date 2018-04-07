a24730cfa41de510c769f0ba52c0cf7f0686eab0
Marshall <xtremebass2k@gmail.com>
2018-04-07 04:22:44 -0700

    Changes to html/js

--- benwaonline/assets.py
+++ benwaonline/assets.py
@@ -4,5 +4,11 @@ assets = Environment()
 css = Bundle('style.css', output='style_busted.%(version)s.css')
 assets.register('css_min', css)
 
+jquery = Bundle('jquery.js')
+assets.register('jquery', jquery)
+
+favicon = Bundle('favicon.js')
+assets.register('favicon', favicon)
+
 js = Bundle('scripts.js')
 assets.register('nice', js)
