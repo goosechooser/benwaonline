6b61df58fb62dccd0756563354d6325f4518c383
Marshall <xtremebass2k@gmail.com>
2018-04-14 06:41:23 -0700

    split schemas up into its own package

---
+++ benwaonline/schemas/__init__.py
@@ -0,0 +1,7 @@
+from benwaonline.schemas.base_schema import BaseSchema
+from benwaonline.schemas.post_schema import PostSchema
+from benwaonline.schemas.preview_schema import PreviewSchema
+from benwaonline.schemas.user_schema import UserSchema
+from benwaonline.schemas.image_schema import ImageSchema
+from benwaonline.schemas.comment_schema import CommentSchema
+from benwaonline.schemas.tag_schema import TagSchema
---
+++ benwaonline/schemas/user_schema.py
@@ -0,0 +1,62 @@
+from marshmallow_jsonapi import fields
+from benwaonline.schemas import BaseSchema
+
+class UserSchema(BaseSchema):
+    id = fields.String()
+    username = fields.String()
+    created_on = fields.DateTime()
+    user_id = fields.String(dump_only=True)
+    active = fields.Boolean(load_from='is_active', dump_to='is_active')
+
+    class Meta:
+        type_ = 'users'
+        strict = False
+        self_url = '/api/users/{id}'
+        self_url_kwargs = {'id': '<id>'}
+        self_url_many = '/api/users'
+
+    comments = fields.Relationship(
+        type_='comments',
+        self_url = '/api/users/{id}/relationships/comments',
+        self_url_kwargs = {'id': '<id>'},
+        related_url = '/api/users/{id}/comments',
+        related_url_kwargs = {'id': '<id>'},
+        many=True,
+        include_resource_linkage=True,
+        schema='CommentSchema'
+    )
+
+    posts = fields.Relationship(
+        type_='posts',
+        self_url = '/api/users/{id}/relationships/posts',
+        self_url_kwargs = {'id': '<id>'},
+        related_url = '/api/users/{id}/posts',
+        related_url_kwargs = {'id': '<id>'},
+        many=True,
+        include_resource_linkage=True,
+        schema='PostSchema'
+    )
+
+    likes = fields.Relationship(
+        dump_only=True,
+        type_='likes',
+        self_url='/api/users/{id}/relationships/likes',
+        self_url_kwargs={'id': '<id>'},
+        related_url='/api/users/{id}/likes',
+        related_url_kwargs={'id': '<id>'},
+        many=True,
+        include_resource_linkage=True,
+        schema='PostSchema'
+    )
+
+    likes = fields.Relationship(
+        load_only=True,
+        type_='posts',
+        self_url='/api/users/{id}/relationships/likes',
+        self_url_kwargs={'id': '<id>'},
+        related_url='/api/users/{id}/likes',
+        related_url_kwargs={'id': '<id>'},
+        many=True,
+        include_resource_linkage=True,
+        schema='PostSchema'
+    )
