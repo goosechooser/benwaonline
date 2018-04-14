622f35a9557b5f74687885fa0c660540e0a2f80c
Marshall <xtremebass2k@gmail.com>
2018-04-14 06:44:47 -0700

    moved all the relationship/uri functionality here

---
+++ benwaonline/mappers.py
@@ -0,0 +1,26 @@
+'''
+'''
+from typing import List
+from marshmallow_jsonapi.fields import Relationship
+
+Entity = 'Entity'
+
+def nonempty_fields(e: Entity) -> List[str]:
+    return [v for v in e.schema._declared_fields.keys() if getattr(e, v)]
+
+def relationships(e: Entity) -> List[str]:
+    return [k for k, v in e.schema._declared_fields.items() if isinstance(v, Relationship)]
+
+def collection_uri(e: Entity) -> str:
+    return e.schema.Meta.self_url_many
+
+def instance_uri(e: Entity) -> str:
+    return e.schema.Meta.self_url.replace('{id}', str(e.id))
+
+def resource_uri(e: Entity, resource: str) -> str:
+    related_url = e.schema._declared_fields[resource].related_url
+    return related_url.replace('{id}', str(e.id))
+
+def relationship_uri(e: Entity, resource: str) -> str:
+    self_url = e.schema._declared_fields[resource].self_url
+    return self_url.replace('{id}', str(e.id))
