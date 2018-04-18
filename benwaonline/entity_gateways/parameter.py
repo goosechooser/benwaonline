074bec13ccf78759854667d3489002f407bdca75
Marshall <xtremebass2k@gmail.com>
2018-04-18 02:54:57 -0700

    Created Parameter object to help clean up function headers

---
+++ benwaonline/entity_gateways/parameter.py
@@ -0,0 +1,52 @@
+import json
+
+class Parameter(object):
+    '''
+    Container for a variety of parameters related to FLASK-REST-JSONAPI/requests.
+
+    Args:
+        include: is a list of strings containing the resources you want included
+        filters: is a list of dicts forming the filter query (see query.py)
+        page_size: is an str related to how many results to return
+        page_number: is the number of the paginated results to get
+        fields[name_of_resource]: is a list containing the fields to return /only/
+
+    '''
+    def __init__(self, include=None, filters=None, page_size=None, page_number=None, fields=None):
+        self._params = {
+            'include': include,
+            'filter': filters,
+            'page_size': page_size,
+            'page_number': page_number,
+            'fields': fields
+        }
+
+    def dump(self):
+        dumping = {
+            'include': ','.join(self.include) if self.include else None,
+            'filter': json.dumps(self.filter) if self.filter else None,
+            'page[size]': self.page_size,
+            'page[number]': self.page_number,
+        }
+
+        if self.fields:
+            f = {'fields[{}]'.format(k): ','.join(v) for k,v in self.fields.items()}
+            dumping.update(f)
+
+        return dumping
+
+    def __getattr__(self, name):
+        if name in self._params:
+            return self._params[name]
+        else:
+            raise AttributeError(name)
+
+    def __setattr(self, name, value):
+        if name in self._params:
+            self._params[name] = value
+        else:
+            raise AttributeError(name)
+
+    @property
+    def params(self):
+        return self._params
---
+++ tests/test_parameter.py
@@ -0,0 +1,36 @@
+import pytest
+from benwaonline import query
+from benwaonline.entities import Post, Tag
+from benwaonline.entity_gateways import Parameter
+
+def test_parameter_init():
+    params = {
+        'page_size': 0,
+        'fields': {'user': ['username']}
+    }
+
+    expected = {
+        'include': 'user,posts',
+        'page[size]': 0,
+        'page[number]': None,
+        'fields[user]': 'username',
+        'filter': None,
+    }
+
+    p = Parameter(include=['user', 'posts'], **params)
+    assert p.dump() == expected
+
+@pytest.mark.skip
+def test_entity_query():
+    tag = Tag(name='hello')
+    tag2 = Tag(name='benwa')
+    post = Post(tags=[tag, tag2])
+
+    c1 = query.EntityCriteria('any', post)
+    q = query.Query(query.Or(c1))
+    expected = {
+        'filter': '[{"or": [{"name": "tags", "op": "any", "val": {"name": "name", "op": "eq", "val": "hello"}}, {"name": "tags", "op": "any", "val": {"name": "name", "op": "eq", "val": "benwa"}}]}]'
+    }
+    p = Parameter(filter=q.dumps())
+    print(p.dump())
+    assert p.dump() == expected
