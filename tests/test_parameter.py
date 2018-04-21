5da8bdffe2ce68cc46df3651dc0ff306639a3481
Marshall <xtremebass2k@gmail.com>
2018-04-21 02:40:34 -0700

    clean up on aisle 5

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -19,18 +19,3 @@ def test_parameter_init():
     #requests ignores key with value of None, so just going to filter them out zzz
     result = {k: v for k,v in p.dump().items() if v is not None}
     assert result == expected
-
-@pytest.mark.skip
-def test_entity_query():
-    tag = Tag(name='hello')
-    tag2 = Tag(name='benwa')
-    post = Post(tags=[tag, tag2])
-
-    c1 = query.EntityCriteria('any', post)
-    q = query.Query(query.Or(c1))
-    expected = {
-        'filter': '[{"or": [{"name": "tags", "op": "any", "val": {"name": "name", "op": "eq", "val": "hello"}}, {"name": "tags", "op": "any", "val": {"name": "name", "op": "eq", "val": "benwa"}}]}]'
-    }
-    p = Parameter(filter=q.dumps())
-    print(p.dump())
-    assert p.dump() == expected
