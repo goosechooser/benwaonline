595d056d0103771e63085ffc6d4c6c5e8a431610
Marshall <xtremebass2k@gmail.com>
2018-05-03 04:16:10 -0700

    datahooks n a better menu/click handler

--- benwaonline/static/scripts.js
+++ benwaonline/static/scripts.js
@@ -1,3 +1,17 @@
+$.extend({
+    hook: function(hookName) {
+        var selector;
+        if(!hookName || hookName === '*') {
+            // select all data-hooks
+            selector = '[data-hook]';
+        } else {
+            // select specific data-hook
+            selector = '[data-hook~="' + hookName + '"]';
+        }
+        return $(selector);
+    }
+});
+
 function handleClick(cb) {
     if ($(cb).is(":checked")) {
         $.post(window.location.pathname + '/like');
@@ -9,16 +23,28 @@ function handleClick(cb) {
     }
 }
 
-function menuHandler() {
-    $("#dropdown").attr('aria-expanded', function(i, expanded) {
-      return expanded === 'false'
-    });
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.menu-button')) {
-      $("#dropdown").attr('aria-expanded', 'false')
+function toggleMenu(menuElement) {
+    const removeClickListener = function() {
+        $(document).off('click', outsideMenuListener)
     }
-  }
 
+    const outsideMenuListener = function(event) {
+        if (!$(event.target).is($(menuElement).prev())) {
+            if($(menuElement).is(':visible')) {
+                $(menuElement).attr('aria-expanded', 'false')
+                $(menuElement).prev().attr('aria-expanded', 'false')
+                removeClickListener()
+            }
+        }
+    }
+
+    $(menuElement).attr('aria-expanded', function(i, expanded) {
+        return expanded === 'false';
+    });
+    $(menuElement).prev().attr('aria-expanded', function(i, expanded) {
+        return expanded === 'false';
+    });
+
+    $(document).on('click', outsideMenuListener)
+
+}
