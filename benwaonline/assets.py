b4ce4e74ccac089e60684527d60b3c5f4fbf4300
Marshall <xtremebass2k@gmail.com>
2018-05-19 01:36:41 -0700

    Merging changes related to deploying new cache busting static content

--- benwaonline/assets.py
+++ benwaonline/assets.py
@@ -1,11 +1,12 @@
+import os
 from flask_assets import Environment, Bundle
 assets = Environment()
 
-css = Bundle('css/style.css', output='style_busted.%(version)s.css')
+css = Bundle(os.getenv('CSS'))
 assets.register('css_min', css)
 
 favicon = Bundle('favicon.js')
 assets.register('favicon', favicon)
 
-js = Bundle('scripts.js')
+js = Bundle(os.getenv('JS'))
 assets.register('scripts', js)
